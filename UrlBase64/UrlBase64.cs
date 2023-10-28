655d1935a95e14d5dd99cf94497b83f4bd7b8cbb
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2023-10-28 10:16:40 -0500

    Perform encoding ourselves, directly into the destination buffer
    We correctly size the destination buffer from the very start, accounting for the
    specified padding policy in advance. This means we don't need to slice the
    output at the end, and we can just return the same buffer we allocated.

--- UrlBase64/UrlBase64.cs
+++ UrlBase64/UrlBase64.cs
@@ -11,10 +11,21 @@ namespace NeoSmart.Utils
 {
     public static class UrlBase64
     {
-        private readonly static char[] DoublePadding = new[] { '=', '=' };
         private const PaddingPolicy DefaultPaddingPolicy = PaddingPolicy.Discard;
 
 #if WITH_SPAN
+        // Forward mapping from any of 64 binary values to their URL-safe Base64 equivalents
+        private readonly static byte[] ToBase64 = new byte[] {
+            (byte)'A', (byte)'B', (byte)'C', (byte)'D', (byte)'E', (byte)'F', (byte)'G', (byte)'H',
+            (byte)'I', (byte)'J', (byte)'K', (byte)'L', (byte)'M', (byte)'N', (byte)'O', (byte)'P',
+            (byte)'Q', (byte)'R', (byte)'S', (byte)'T', (byte)'U', (byte)'V', (byte)'W', (byte)'X',
+            (byte)'Y', (byte)'Z', (byte)'a', (byte)'b', (byte)'c', (byte)'d', (byte)'e', (byte)'f',
+            (byte)'g', (byte)'h', (byte)'i', (byte)'j', (byte)'k', (byte)'l', (byte)'m', (byte)'n',
+            (byte)'o', (byte)'p', (byte)'q', (byte)'r', (byte)'s', (byte)'t', (byte)'u', (byte)'v',
+            (byte)'w', (byte)'x', (byte)'y', (byte)'z', (byte)'0', (byte)'1', (byte)'2', (byte)'3',
+            (byte)'4', (byte)'5', (byte)'6', (byte)'7', (byte)'8', (byte)'9', (byte)'-', (byte)'_'
+        };
+
         // Reverse mapping of base64 alphabet to numerical value, such that table[(byte)'A'] = 0, ...
         private readonly static byte[] FromBase64 = new byte[] {
             0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
@@ -50,6 +61,8 @@ namespace NeoSmart.Utils
             0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
             0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF
         };
+#else
+        private readonly static char[] DoublePadding = new[] { '=', '=' };
 #endif
 
         static UrlBase64()
@@ -116,38 +129,68 @@ namespace NeoSmart.Utils
         public static byte[] EncodeUtf8(ReadOnlySpan<byte> input, PaddingPolicy padding = DefaultPaddingPolicy)
         {
             // Every three input bytes become 4 output bytes, and there are possibly two bytes of padding
-            int length = (input.Length + 2) / 3 * 4;
-            Debug.Assert(length == Base64.GetMaxEncodedToUtf8Length(input.Length));
-            Span<byte> utf8 = length <= 1024 ? stackalloc byte[length] : new byte[length];
-            Base64.EncodeToUtf8(input, utf8, out var bytesRead, out var bytesWritten, isFinalBlock: true);
-            Debug.Assert(length == bytesWritten);
-            Debug.Assert(bytesRead == input.Length);
+            int maxLength = (input.Length + 2) / 3 * 4;
+            Debug.Assert(maxLength == Base64.GetMaxEncodedToUtf8Length(input.Length));
+            int paddingLength = (3 - (input.Length % 3)) % 3;
+            int length = (padding == PaddingPolicy.Preserve) ? maxLength : maxLength - paddingLength;
+            var utf8 = new byte[length];
 
-            // .Replace('+', '-').Replace('/', '_')
-            for (int i = length - 1; i >= 0; --i)
+            // Read three bytes at a time, which give us four bytes of base64-encoded output
+            int i = 0;
+            int j = 0;
+            for (; i + 3 <= input.Length; i += 3)
             {
-                utf8[i] = utf8[i] switch
-                {
-                    (byte)'+' => (byte)'-',
-                    (byte)'/' => (byte)'_',
-                    byte b => b,
-                };
+                uint threeBytes = ((uint)(input[i] << 16)) | ((uint)(input[i + 1] << 8)) | input[i + 2];
+                uint fourBytes =
+                      (((uint)ToBase64[(threeBytes >> 0) & 0x3F]) << 24)
+                    | (((uint)ToBase64[(threeBytes >> 6) & 0x3F]) << 16)
+                    | (((uint)ToBase64[(threeBytes >> 12) & 0x3F]) << 8)
+                    | (ToBase64[threeBytes >> 18]);
+                utf8[j++] = (byte)fourBytes;
+                utf8[j++] = (byte)(fourBytes >> 8);
+                utf8[j++] = (byte)(fourBytes >> 16);
+                utf8[j++] = (byte)(fourBytes >> 24);
             }
 
-            if (padding == PaddingPolicy.Discard)
+            // Handle the remaining bytes not divisible by 3
+            if (i < input.Length)
             {
-                // Max padding in standard base64 is two trailing equal signs
-                if (utf8.Length > 1 && utf8[utf8.Length - 2] == (byte)'=')
+                switch (input.Length - i)
                 {
-                    return utf8.Slice(0, utf8.Length - 2).ToArray();
+                    case 2:
+                        {
+                            int num = (input[i] << 16) | (input[i + 1] << 8);
+                            i += 2;
+                            var bytes = (ToBase64[(num >> 6) & 0x3F] << 16)
+                                | (ToBase64[(num >> 12) & 0x3F] << 8)
+                                | ToBase64[num >> 18];
+                            utf8[j++] = (byte)(bytes >> 0);
+                            utf8[j++] = (byte)(bytes >> 8);
+                            utf8[j++] = (byte)(bytes >> 16);
+                            break;
+                        }
+                    case 1:
+                        {
+                            int oneByte = input[i++] << 8;
+                            var bytes = (ToBase64[(oneByte >> 4) & 0x3F] << 8) | ToBase64[oneByte >> 10];
+                            utf8[j++] = (byte)bytes;
+                            utf8[j++] = (byte)(bytes >> 8);
+                            break;
+                        }
                 }
-                else if (utf8.Length > 0 && utf8[utf8.Length - 1] == (byte)'=')
+
+                // Add padding if necessary
+                if (padding == PaddingPolicy.Preserve)
                 {
-                    return utf8.Slice(0, utf8.Length - 1).ToArray();
+                    for (int k = 0; k < paddingLength; k++)
+                    {
+                        utf8[j++] = (byte)'=';
+                    }
                 }
             }
+            Debug.Assert(i == input.Length);
 
-            return utf8.ToArray();
+            return utf8;
         }
 
         public static byte[] Decode(ReadOnlySpan<char> input)
